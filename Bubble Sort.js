2acc4751ef922f00db6df3d311ef9393e3b95d4d
moneer-Khaled <156200343+moneer-Khaled@users.noreply.github.com>
2025-06-19 15:38:22 +0200

    Bubble Sort.js

--- Bubble Sort.js
+++ Bubble Sort.js
@@ -1,17 +1,19 @@
-function bubbleSort(arr){
+function bubbleSort(arr) {
     let n = arr.length;
 
-
-    for( let i = 0; i<n-1; i++){
-        for(let j = 0; j<n-1-i; j++){
-            if(arr[j]> arr[j+1]){
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
                 let x = arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=x;
-
+                arr[j] = arr[j + 1];
+                arr[j + 1] = x;
             }
         }
     }
-    return x;
+
+    return arr;  
 }
-console.log(bubbleSortDescending([5, 1, 4, 2,55,634,575,553,66,]));
+
+console.log(bubbleSort([5, 1, 4, 2, 55, 634, 575, 553, 66]));
+
+console.log(bubbleSort([7,5,2,4,3,9]));
